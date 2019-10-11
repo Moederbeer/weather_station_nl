5ef0bc4e52452d5444be6d1afd30eea9cc739530
Paul de Groot <pauldgroot@gmail.com>
2019-10-11 09:14:35 +0200

    First commit

---
+++ venv/Scripts/easy_install-3.7-script.py
@@ -0,0 +1,12 @@
+#!"C:\Users\pauld\Google Drive\Jaar 2.5\OOP\Opdrachten\weather_station_nl\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install-3.7'
+__requires__ = 'setuptools==40.8.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==40.8.0', 'console_scripts', 'easy_install-3.7')()
+    )
