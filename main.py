dffde775748ad29e5ea14b50a59deae5e0bd438c
Paul de Groot <pauldgroot@gmail.com>
2019-10-11 10:37:09 +0200

    Getting JSON from buienradar

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import libu
+import urllib.request
+import json
+
+
+def main():
+    # webUrl = urllib.request.urlopen("https://data.buienradar.nl/2.0/feed/json")
+    webUrl =  open('sample_json')
+    weatherData = json.load(webUrl)["actual"]
+
+    for s in weatherData["stationmeasurements"]:
+        print(s)
+
+main()
